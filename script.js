d5d562626e21c482b28804df1f534f5be21b2721
Harry Northam <hello@harrynortham.com>
2024-07-24 10:44:14 +0200

    click eventlistener on squares now triggers takeTurn function

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
+// player factory function
+//createPlayer
+// name, marker
+
 // using module pattern: IIFEs because we only need to use once
 
 const displayController = (function () {
-  const takeTurn = console.log("turn taken");
+  const takeTurn = function (i) {
+    console.log("clicked " + i);
+  };
   return { takeTurn };
 })();
 
 const gameBoard = (function () {
+  const { takeTurn } = displayController; // give access to the takeTurn function with destructuring
   const boardContainer = document.getElementById("game-board-container");
   const gameBoard = document.createElement("div");
   gameBoard.id = "game-board";
-  gameBoard.innerText = "This is a paragraph";
-  // the tiles function is not returned, therefore private
-  const tiles = () => {
-    const tileCount = 9;
-    const tile = document.createElement("div");
-    //loop through tile count and create new element for each tile
-    //attach click event listener to each tile and trigger display controller function, we will work from there
-    // add all tile elements to the gameBoard
+  // the addSquares function is not returned, therefore private
+  const addSquares = () => {
+    const squareCount = 9;
+    //loop through square count and create new element for each square
+    for (let i = 0; i < squareCount; i++) {
+      const square = document.createElement("div");
+      square.classList.add("game-square", `game-square--${i}`);
+      // attach click event listener to each square and trigger display controller function, we will work from there
+      square.addEventListener("click", () => {
+        takeTurn(i);
+        // if takeTurn returns marker X or O we will add element to the div
+      });
+      // add square element to the gameBoard
+      gameBoard.appendChild(square);
+    }
   };
-  const create = () => {
+
+  const createBoard = () => {
+    addSquares();
     boardContainer.appendChild(gameBoard);
   };
-  return { create };
+  return createBoard();
+  //return {create};
+  //or if we don't want it run immediately, or return multiple functions we can call the gameBoard.create() later
 })();
 
-gameBoard.create();
+//create a start game function that create a board and two player objects
+
+//create a reset game function
